cd7f913f26ebcb0923f96336e07c1bea04a122cf
Chetan Patil <10114477+abhiyaantrix@users.noreply.github.com>
2024-09-15 13:49:07 +0200

    feat: Add TailwindCSS and daisyUI (#4)
    - Add and configure TailwindCSS along with standard plugins
    - Add daisyUI and configure
    - Add postcss and autoprefixer

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Event Radar
+      </h1>
+      <button className="btn btn-primary">Dummy daisyUI Button</button>
+    </>
+  )
+}
+
+export default App
